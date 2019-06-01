1c75d261e4970d0898bb2c38564c81fdf3c151c4
Oscar Ko <OscarKoEmail@gmail.com>
2019-06-01 12:11:10 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// eshint jsversion: 6
+
+// declare required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+// declare uses and sets
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+
+// app gets, posts, listens
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
